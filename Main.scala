0223eb6ebbe02a5ef75365eabdec2ce969521202
zabotio <simon9817@gmail.com>
2016-10-12 14:42:39 +0200

    Update Main.scala
    Added a main function which will do most of the competition work

--- Main.scala
+++ Main.scala
@@ -1,7 +1,28 @@
 package turtlerace
+import scala.collection.mutable.ArrayBuffer;
 
 object Main {
   def main(args : Array[String]): Unit = {
-    println("HELLO");
+    val window = new RaceWindow(800, 400, "Turtlerace");
+    var turtles = for(i <- 0 to 31) yield randTurtle(window, i, "Turtle " + i);
+    var activeSeq: Seq[RaceTurtle];
+    var winners: ArrayBuffer[RaceTurtle];
+    var i=1;
+    while(i<=4){
+      var x = -1;
+      while(x<32/i){
+        activeSeq = for(j <- 0 until 8) yield{
+          x+=1;
+          turtles(x).asInstanceOf[RaceTurtle];
+        }
+        winners = TurtleRace.race(activeSeq, window, "Tournament part " + i).to[ArrayBuffer];
+      }
+      turtles = winners.toIndexedSeq;
+      i*=2;
+    }
+  }
+
+  def randTurtle(window: RaceWindow, num: Int, name: String): RaceTurtle = {
+
   }
 }
